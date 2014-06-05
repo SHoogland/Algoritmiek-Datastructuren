a6d4a6ed6c68d4a70c2f54f748ddd8ad6840359e
Stephan <stephan_hoogland5@hotmail.com>
2014-06-05 23:08:14 +0200

    Redundancy Fail!!!!

--- Prim Alghorithm/Form1.cs
+++ Prim Alghorithm/Form1.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Linq;
 
 namespace Prim_Alghorithm {
     public partial class Form1 : Form {
